b110b2893e27f7d9dba0714c1d03b99a9f5146de
davidblancolopez <daviddbl94@gmail.com>
2017-01-11 17:35:44 +0100

    CAMBIOS.

--- src/activitat2/pkg1/m9/uf2/GameWorld.java
+++ src/activitat2/pkg1/m9/uf2/GameWorld.java
@@ -6,8 +6,19 @@ package activitat2.pkg1.m9.uf2;
  * @author ALUMNEDAM
  */
 public class GameWorld implements Runnable{
+    Ball list;
     
+    public GameWorld (){
+    
+    }
 
+    public void startSimulation(){
+        
+    }
+    
+    public void endSimulation(){
+        
+    }
     
     public void run(){
        
